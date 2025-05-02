b33b20d4659b9a923ef667177a25db3d40a9d5f6
amnk71 <alotaibaamna71@gmail.com>
2025-05-02 23:00:31 +0400

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,28 +1,31 @@
 mod lexer;
 mod parser;
 mod vm;
+mod error;
 
 use lexer::Lexer;
-use parser::Parser;
+use parser::{Parser, Expr};
 use vm::{VM, Instruction};
+use error::CompileError;
 
-fn main() {
+fn main() -> Result<(), CompileError> {
     let source = "1 + 2 * 3";
     let lexer = Lexer::new(source);
-    let mut parser = Parser::new(lexer);
-    let expr = parser.parse_expr();
-
+    let mut parser = Parser::new(lexer)?; 
+    let expr = parser.parse_expr()?;      
+    
     // Compile to VM instructions
     let code = compile_expr(expr);
     let mut vm = VM::new(code);
     let result = vm.run();
-    println!("Result: {}", result); // Output: 7
+    println!(" Result: {}", result);
+    Ok(())
 }
 
-fn compile_expr(expr: parser::Expr) -> Vec<Instruction> {
+fn compile_expr(expr: Expr) -> Vec<Instruction> {
     match expr {
-        parser::Expr::Number(n) => vec![Instruction::Push(n)],
-        parser::Expr::BinaryOp(left, op, right) => {
+        Expr::Number(n) => vec![Instruction::Push(n)],
+        Expr::BinaryOp(left, op, right) => {
             let mut code = compile_expr(*left);
             code.extend(compile_expr(*right));
             match op {
@@ -34,6 +37,8 @@ fn compile_expr(expr: parser::Expr) -> Vec<Instruction> {
             }
             code
         }
-        _ => panic!("Unsupported expression"),
+        Expr::Variable(name) => {
+            panic!(" Variable '{}' not supported in this stage", name);
+        }
     }
 }
