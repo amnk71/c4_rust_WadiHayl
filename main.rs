1545993b9edce0b2bb31023a25ed7f9ad48744b9
amnk71 <alotaibaamna71@gmail.com>
2025-05-03 00:05:13 +0400

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,3 +1,6 @@
+//! Entry point for the C4 Rust compiler.
+//! Runs the lexer, parser, and virtual machine on a sample expression.
+
 mod lexer;
 mod parser;
 mod vm;
@@ -8,20 +11,27 @@ use parser::{Parser, Expr};
 use vm::{VM, Instruction};
 use error::CompileError;
 
+/// Main execution flow: lex, parse, compile, and run an arithmetic expression.
+/// Returns `Ok(())` on success or a `CompileError` if any stage fails.
 fn main() -> Result<(), CompileError> {
     let source = "1 + 2 * 3";
     let lexer = Lexer::new(source);
     let mut parser = Parser::new(lexer)?; 
     let expr = parser.parse_expr()?;      
-    
+
     // Compile to VM instructions
     let code = compile_expr(expr);
     let mut vm = VM::new(code);
     let result = vm.run();
-    println!(" Result: {}", result);
+    println!("Result: {}", result);
     Ok(())
 }
 
+/// Recursively compiles an expression into a vector of virtual machine instructions.
+/// # Arguments
+/// * `expr` - An abstract syntax tree representing the parsed expression.
+/// # Returns
+/// A vector of `Instruction`s to be executed by the VM.
 fn compile_expr(expr: Expr) -> Vec<Instruction> {
     match expr {
         Expr::Number(n) => vec![Instruction::Push(n)],
@@ -38,7 +48,7 @@ fn compile_expr(expr: Expr) -> Vec<Instruction> {
             code
         }
         Expr::Variable(name) => {
-            panic!(" Variable '{}' not supported in this stage", name);
+            panic!("Variable '{}' not supported in this stage", name);
         }
     }
 }
